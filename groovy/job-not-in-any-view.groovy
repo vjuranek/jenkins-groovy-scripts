592cbeaedc78a05470f010841fcc5c74c75c915b
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-02-09 14:31:04 +0100

    Add script for listing jobs not in any view

---
+++ groovy/job-not-in-any-view.groovy
@@ -0,0 +1,37 @@
+import jenkins.model.Jenkins;
+import hudson.model.*
+
+public void addAllJobs(View view, Set<String> jobs){
+  if(view instanceof ViewGroup) {
+    for(View v: view.getViews()) {
+      addAllJobs(v, jobs);
+    }
+    if(view.getItems()!=null) {
+      for(Item i: view.getItems()) {
+        jobs.add(i.getDisplayName());
+      }
+    }
+  }
+  else {
+     if(!(view instanceof ProxyView) && !view.name.equals("All")) {
+      for(Item i: view.getItems()){
+        jobs.add(i.getDisplayName());
+      }
+     }
+  }
+}
+
+Set jobsInViews = new HashSet()
+Jenkins.instance.views.each {
+  addAllJobs(it, jobsInViews)
+}
+
+println("=====================")
+println("Jobs not in any view:")
+Jenkins.instance.items.each {
+  if (!jobsInViews.contains(it.displayName))
+    println(it.displayName)
+}
+println("=====================")
+
+return null
