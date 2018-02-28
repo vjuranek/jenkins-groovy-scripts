cc9692b9453dbc34c82bd5be2962442185f55295
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-02-28 20:55:05 +0100

    Comment additional rsync line

--- bash/simple-git-backup.sh
+++ bash/simple-git-backup.sh
@@ -6,6 +6,9 @@ GIT_DIR=/jenkins_backup
 cd $GIT_DIR
 git pull --rebase origin master
 rsync -aR $JENKINS_DIR/./jobs/*/config.xml $GIT_DIR
+#ensure deleted projects are removed,
+#previous line uses * which lists all exiting dirs, but not deleted ones
+#and thus using --delete on previous line has no effect
 rsync -dR --delete $JENKINS_DIR/./jobs/ $GIT_DIR
 rsync -a --delete $JENKINS_DIR/*.xml $GIT_DIR
 git add --all $GIT_DIR
