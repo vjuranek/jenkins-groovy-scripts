d7e6ee461a23ab4a380562fcbd2f18411e80808e
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-03-22 11:18:15 +0100

    Add also example for deleting aborted builds

--- groovy/delete-failed-builds.groovy
+++ groovy/delete-failed-builds.groovy
@@ -1,6 +1,7 @@
 JOB_NAME = "infinispan-spark"
 
 Jenkins.instance.getItem(JOB_NAME).builds.each {
+  //if(hudson.model.Result.FAILURE == it.getResult() || hudson.model.Result.ABORTED == it.getResult()){
   if(hudson.model.Result.FAILURE == it.getResult()){
     println "Deleteing build " + it
     it.delete()
