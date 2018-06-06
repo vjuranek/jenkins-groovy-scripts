11e52975cec835e552dca3f34ac23239fb831add
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-06-06 23:01:20 +0200

    Add script which deltes builds older then specified ID

---
+++ groovy/delete-old-builds.groovy
@@ -0,0 +1,11 @@
+JOB_NAME = "test"
+MIN_JOB = 100
+
+Jenkins.instance.getItem(JOB_NAME).builds.each {
+  if (it.id.toInteger() < MIN_JOB) {
+    println "Deleteing build " + it.id
+    it.delete()
+  }
+}
+
+return null
